15e9e91869cbebed540aed28c2a6b84ffaaa5496
Renato <tato_ap@hotmail.com>
2022-03-24 16:17:14 -0300

    JSX também é uma expressão

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,5 +1,12 @@
 export default function HelloWorld() {
     const name = 'Renato'
+    const title = <h1>Hello, {name}!</h1>
+
+    if (name === 'Renato') {
+        return <div>
+            <h1 style={{ background: 'red' }}>{ name }</h1>
+        </div>
+    }
 
     return <div>
         <h1>Hello, { name === 'Renato' ? 'Henrique' : name }!!</h1>
