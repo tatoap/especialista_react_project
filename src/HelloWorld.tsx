03db2a00304e0818b145274de43b525ee9071aae
Renato <tato_ap@hotmail.com>
2022-03-24 16:54:13 -0300

    Atributos com JSX

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,14 +1,19 @@
 export default function HelloWorld() {
     const name = 'Renato'
     const title = <h1>Hello, {name}!</h1>
+    const index = 0
 
     if (name === 'Renato') {
         return <div>
-            <h1 style={{ background: 'red' }}>{ name }</h1>
+            <h1 tabIndex={ index } style={{ background: 'red' }}>{ name }</h1>
         </div>
     }
 
     return <div>
         <h1>Hello, { name === 'Renato' ? 'Henrique' : name }!!</h1>
     </div>
+
+    //return <div>
+    //    { title }
+    //</div>
 }
