c5714b71199a02762900b9e8498d9f86bca0f305
Renato <tato_ap@hotmail.com>
2022-03-29 18:49:59 -0300

    Atualizando um componente em tempo real

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-
-const Component = React
-    .createElement('div', { style: { background: 'red' } }, 'Olá, mundo')
-
-const HTMLElement = document.createElement('div')
-HTMLElement.style.background = 'red'
-HTMLElement.innerText = 'Olá, mundo!'
-
-console.log(Component);
-console.log(HTMLElement);
-
-// @ts-ignore
-// document.body.appendChild(Component)
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        { Component }
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá mundo!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Clock />
       </header>
     </div>
   );
