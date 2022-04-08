e1409a6ff4cdda97607a28f100d853031163cc5b
Renato <tato_ap@hotmail.com>
2022-04-08 17:42:16 -0300

    Recuperando atualizações do estado com componentDidUpdate

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,6 @@ interface ButtonState {
 
 class Button extends Component<ButtonProps, ButtonState> {
     constructor (props: ButtonProps) {
-        console.log('constructor')
         super(props)
 
         this.state = {
@@ -19,13 +18,21 @@ class Button extends Component<ButtonProps, ButtonState> {
         }
     }
 
-    componentDidMount () {
+    /*componentDidMount () {
         setTimeout(() => {
             this.setState({
                 alreadyClicked: false
             })
             console.log('rodou o timeout')
         }, 5000)
+    }*/
+
+    componentDidUpdate (prevProps: ButtonProps, prevState: ButtonState) {
+        console.log('anterior', prevState)
+        console.log('atual', this.state)
+
+        //console.log('anterior', prevProps)
+        //console.log('atual', this.props)
     }
 
     render () {
