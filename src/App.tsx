be485f93ea4349f80bd9fb7a8af582e156ac746d
Renato <tato_ap@hotmail.com>
2022-04-12 13:36:30 -0300

    Renderizando listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { toNamespacedPath } from 'node:path/win32';
 import './App.css';
 import Form from './components/Form';
 
 function App() {
+  const names = ['Renato', 'Henrique', 'Eliana', 'Roseli']
 
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
+        <ul>
+          {
+            names.map((name, index) => <li key={index}>{name}</li>)
+          }
+        </ul>
       </header>
     </div>
   );
